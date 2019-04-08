6170b47f01d7a0f07a3c1f05223d9cfe8774c94e
Paul <paul.m.schaefer@gmx.de>
2019-04-08 15:07:46 +0200

    Fixed volume problems

--- game_sound.py
+++ game_sound.py
@@ -1,3 +1,4 @@
+import time
 import random
 import pygame
 
@@ -6,22 +7,26 @@ _songs = ['./sound-files/lied.mp3', './sound-files/lied2.mp3']
 
 def play_song(_song_file_name):
     pygame.mixer.music.load(_song_file_name)
+    pygame.mixer.music.set_volume(0.2)
     print('Song playing:' + _song_file_name)
     pygame.mixer.music.play(0)
 
 
 def play_sound(file='./sound-files/effects/bell.wav'):
     sound = pygame.mixer.Sound(file)
+    sound.set_volume(1.0)
     pygame.mixer.Channel(1).play(sound)
     print('Sound playing:' + file)
+
 
 
 def main():
     pygame.init()
     pygame.mixer.init()
     pygame.mixer.music.set_endevent(pygame.QUIT)
+
     play_sound()
-    pygame.time.wait(600)
+    pygame.time.wait(500)
     play_song(random.choice(_songs))
     while True:
         for event in pygame.event.get():
