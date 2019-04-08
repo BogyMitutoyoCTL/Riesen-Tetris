31c3d96bb2eb4b0095d944d42c1697defd6afc76
Paul <paul.m.schaefer@gmx.de>
2019-04-08 14:38:55 +0200

    added channels and fixed bugs

--- game_sound.py
+++ game_sound.py
@@ -10,18 +10,27 @@ def play_song(_song_file_name):
     pygame.mixer.music.play(0)
 
 
+def play_sound(file='./sound-files/effects/bell.wav'):
+    sound = pygame.mixer.Sound(file)
+    pygame.mixer.Channel(1).play(sound)
+    print('Sound playing:' + file)
+
+
 def main():
     pygame.init()
     pygame.mixer.init()
     pygame.mixer.music.set_endevent(pygame.QUIT)
+    play_sound()
+    pygame.time.wait(600)
     play_song(random.choice(_songs))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 next_song = random.choice(_songs)
                 play_song(next_song)
-            else:
-                print(event)
+                play_sound()
+#todo: Wire up sounds with events
+
 
 
 if __name__ == '__main__':
