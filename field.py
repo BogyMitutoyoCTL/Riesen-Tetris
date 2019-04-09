b7a98a7d650cf719be9f953c7c02a3b3782cf233
Jojo-me <jojo.rw14@gmail.com>
2019-04-09 13:40:05 +0200

    Tut. Man kann einen Block auf beide Felder malen

--- field.py
+++ field.py
@@ -1,6 +1,5 @@
-from block import blocks
-from random import random
-from array_vervielfachen import Array_Vervielfachen
+from block import Block
+
 
 class Field:
     def __init__(self, width, height):
@@ -8,7 +7,8 @@ class Field:
         self.height = height
         self.field = []
         self.generate_field()
-        self.colors = [[170, 0, 255], [255, 225, 0], [0, 255, 245], [20, 255, 0], [255, 0, 0], [255, 165, 0], [0, 40, 255]]
+        self.colors = [[170, 0, 255], [255, 225, 0], [0, 255, 245], [20, 255, 0], [255, 0, 0], [255, 165, 0],
+                       [0, 40, 255]]
 
     def set_all_pixels_to_black(self):
         for y in range(0, self.height):
@@ -24,20 +24,14 @@ class Field:
             for x in range(0, self.width):
                 self.field[y].append([0, 0, 0])
 
-    def set_block(self, x=0, y=0):
-        blocktodraw = blocks[int(random()*7)]
+    def set_block(self, block_to_draw: Block, x=3, y=0):
         line_number = 0
-        for line in blocktodraw:
+        for line in block_to_draw.pixels:
             column_number = 0
             for column in line:
                 if column != 0:
-                    self.set_pixel(column_number+x, line_number+y, blocktodraw[line_number][column_number]-1)
-                column_number = column_number+1
-            line_number = line_number+1
-
-
-
-    def set_block_doppel_size(self, x=0, y=0):
-        array_vervielfachen = Array_Vervielfachen()
-        blocktodraw = array_vervielfachen.resize_dopple(blocks[int(random()*7)])
-
+                    if block_to_draw.pixels[line_number][column_number] > 0:
+                        self.set_pixel(column_number + x, line_number + y,
+                                       block_to_draw.pixels[line_number][column_number] - 1)
+                column_number = column_number + 1
+            line_number = line_number + 1
