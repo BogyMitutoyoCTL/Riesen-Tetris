e9b8d64055d1df759afae8d16b0807a662adbd4f
flobuschi <1107200320030711>
2019-04-10 13:18:28 +0200

    highscoreliste

--- highscorelist.py
+++ highscorelist.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from datetime import date
-from typing import *
+import pickle
+
 
 class Highscoreentry:
     date = 0
@@ -23,19 +24,39 @@ class Highscoreentry:
 
 
 class Highscorelist:
-    def __init__(self):
+    def __init__(self, filename):
         self.highscores = []
+        self.filename = filename
 
     def add_entry(self, entry: Highscoreentry):
         self.highscores.append(entry)
         self.highscores.sort(reverse=True)
         self.highscores[10:20] = []
 
+    def save(self, ):
+        with open(self.filename, 'wb') as f:
+            pickle.dump(self, f)
 
-highscorelist = Highscorelist()
-today = date.today()
-print(today)
-for i in range(20):
-    x = Highscoreentry(today, "Florian", i)
-    highscorelist.add_entry(x)
-print(highscorelist.highscores)
+    def load(self):
+        with open(self.filename, 'rb') as f:
+            temporary_list = pickle.load(f)
+            self.highscores = temporary_list.highscores
+
+
+if __name__ == "__main__":
+    snakehighscores = Highscorelist('snakescores')
+    snakehighscores.save()
+    tetrishighscores = Highscorelist('tetrisscores')
+
+    today = date.today()
+    print(today)
+    for i in range(20):
+        x = Highscoreentry(today, "Florian", i)
+        tetrishighscores.add_entry(x)
+    tetrishighscores.save()
+    tetrishighscores.save()
+    print(tetrishighscores.highscores)
+
+    x = Highscorelist('tetrisscores')
+    x.load()
+    print(x.highscores)
