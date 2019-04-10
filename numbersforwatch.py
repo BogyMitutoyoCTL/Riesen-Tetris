e40607ad0f29f1fe777786c7087d0675c5177f8a
BenniGref <benni03223gmail.com>
2019-04-10 11:35:01 +0200

    number on rgb_field

--- numbersforwatch.py
+++ numbersforwatch.py
@@ -1,6 +1,8 @@
 from block import Block
 from field import Field
 from painter import RGB_Field_Painter
+from datetime import datetime, date
+
 number_pixels = [
 
     [[1, 1, 1, 1],
@@ -69,27 +71,51 @@ number_colors = [[255, 255, 255], [51, 255, 255], [255, 0, 0], [50, 255, 50], [1
 
 
 class Number:
-
     def __init__(self, number: int):
         self.pixel = number_pixels[number]
         self.color = number_colors[number]
         self.block = Block(self.pixel, self.color)
 
-zero = Number(0)
-one = Number(1)
-two = Number(2)
-three = Number(3)
+class Clock:
+    def __init__(self, field_for_clock):
+        self.number_list = [Number(0), Number(1), Number(2), Number(3), Number(4), Number(5), Number(6), Number(7),
+                            Number(8), Number(9)]
+        self.field_for_clock = field_for_clock
+
+    def get_time(self):
+        tuple_time = datetime.timetuple(datetime.today())
+        print(tuple_time)
+        # self.hour = tuple_time[3]
+        self.hour = 9
+        print(tuple_time[4])
+        print(tuple_time[5])
+        self.draw_clock()
+
+    def draw_clock(self):
+        self.hour_str = ""
+        if self.hour < 10:
+            self.hour_str += "0"
+        self.hour_str += str(self.hour)
+        print(self.hour_str)
+        self.number_array = [int(self.hour_str[0]), int(self.hour_str[1])]
+        self.field_for_clock.set_block(self.number_list[self.number_array[0]].block, 0, 1)
+        self.field_for_clock.set_block(self.number_list[self.number_array[1]].block, 5, 1)
 
 
 field_leds = Field(10, 20)
 rgb_field_painter = RGB_Field_Painter()
 
-field_leds.set_block(two.block, 0, 1)
-field_leds.set_block(two.block, 5, 1)
-field_leds.set_block(two.block, 0, 7)
-field_leds.set_block(two.block, 5, 7)
-field_leds.set_block(two.block, 0, 13)
-field_leds.set_block(two.block, 5, 13)
+clock = Clock(field_leds)
+
+# field_leds.set_block(number_list[0].block, 0, 1)
+# field_leds.set_block(number_list[1].block, 5, 1)
+# field_leds.set_block(zero.block, 5, 1)
+# field_leds.set_block(four.block, 0, 7)
+# field_leds.set_block(five.block, 5, 7)
+# field_leds.set_block(two.block, 0, 13)
+# field_leds.set_block(eight.block, 5, 13)
+
+clock.get_time()
 rgb_field_painter.draw(field_leds)
 
 input()
