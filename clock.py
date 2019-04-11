8f3227ae0fc6a92e1f499d8a595b9e832cdbf62e
Jojo <jojo.rw14@gmail.com>
2019-04-11 21:39:21 +0200

    Uhr zeigt Datum in LED-Matrix an

--- clock.py
+++ clock.py
@@ -15,6 +15,10 @@ class Clock(Feature):
         tuple_time = datetime.timetuple(datetime.today())
         return tuple_time[3], tuple_time[4],  tuple_time[5]
 
+    def get_date(self):
+        tuple_date = datetime.timetuple(datetime.today())
+        return tuple_date[0], tuple_date[1],  tuple_date[2]
+
     def draw_clock(self):
         self.field_leds.set_all_pixels_to_black()
         hour, minute, second = self.get_time()
@@ -32,12 +36,23 @@ class Clock(Feature):
 
         self.rgb_field_painter.draw(self.field_leds)
 
+    def draw_date(self):
+        self.field_matrix.set_all_pixels_to_black()
+        year, month, day = self.get_date()
+        day_str = ("0"+str(day))[-2:]
+        month_str = ("0" + str(month))[-2:]
+        year_str = ("000" + str(year))[-4:]
+
+        print(day_str+"."+month_str+"."+year_str)
+        self.led_matrix_painter.show_Text(""+day_str+"."+month_str+"."+year_str+"")
+
     def event(self, eventname: str):
         if eventname == "break":
             pass
 
     def tick(self):
         self.draw_clock()
+        self.draw_date()
         time.sleep(0.2)
 
     def start(self):
