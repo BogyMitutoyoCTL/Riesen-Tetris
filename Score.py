226e211a89d5c3bde711a20919f04e6400e03ddd
flobuschi <1107200320030711>
2019-04-11 12:43:35 +0200

    score ansatz

--- Score.py
+++ Score.py
@@ -1,15 +1,32 @@
 
-class Scorelist:
-    def __init__(self, point):
-        self.point = point
-    def score(self, point):
+class Score:
+    def __init__(self):
+        self.point = 0
+    def score_for_line(self, deleted_lines):
+        score.score_for_line(deleted_lines)
+        pass
 
 
-#add dictionary
-#add function for score
+    def score_for_block(self):
+        self.point +=1
 
 
 
-scorelist = Scorelist()
 
 
+
+
+score = Score()
+assert(score.point == 0)
+score.score_for_block()
+assert (score.point ==1)
+score.score_for_line(0)
+assert (score.point ==1)
+score.score_for_line(1)
+assert (score.point ==11)
+score.score_for_line(2)
+assert (score.point == 51)
+score.score_for_line(3)
+assert (score.point ==1 41)
+score.score_for_line(4)
+assert (score.point == 341)
