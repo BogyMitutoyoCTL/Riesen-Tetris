a22608e08c050db0ddaad249f6858fd33c4e0550
Thomas Weller <thomas.weller@mitutoyo-ctl.de>
2019-04-25 15:50:15 +0200

    Move Java check to Pycharm installation Fix: use cat instead of echo

--- check.sh
+++ check.sh
@@ -3,26 +3,6 @@
 reboot_required=no
 connect_usbsound=no
 
-# Make Java compatible with PyCharm
-if [ "`java -version 2>&1 | grep version | grep 1.8.0_65 | wc -l`" -eq "1" ]; then
-    echo -e "\e[91mJava is problematic Oracle version 1.8.0.65.\e[39m"
-    echo -e "\e[96mSwitching to OpenJDK Ice-T version.\e[39m"
-    apt-get purge -y oracle*
-    apt-get update
-    apt-get upgrade
-    apt-get remove -y oracle*
-    apt-get install -y openjdk-8-jre
-    if [ "`java -version 2>&1 | grep version | grep 1.8.0_65 | wc -l`" -eq "0" ]; then
-        echo -e "\e[92mJava update seems good.\e[39m"
-    else
-        echo -e "\e[91mJava is still 1.8.0_65.\e[39m"
-        echo -e "\e[96mDo you have an active Internet connection?\e[39m"
-        exit 3
-    fi
-else
-    echo -e "\e[92mJava is not the broken version 1.8.0.65.\e[39m";
-fi
-
 # Back up config files
 cp /boot/config.txt /boot/config.txt$(date +%Y%m%d)
 cp /usr/share/alsa/alsa.conf /usr/share/alsa/alsa.conf$(date +%Y%m%d)
--- pycharm.sh
+++ pycharm.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+# Make Java compatible with PyCharm
+if [ "`java -version 2>&1 | grep version | grep 1.8.0_65 | wc -l`" -eq "1" ]; then
+    echo -e "\e[91mJava is problematic Oracle version 1.8.0.65.\e[39m"
+    echo -e "\e[96mSwitching to OpenJDK Ice-T version.\e[39m"
+    apt-get purge -y oracle*
+    apt-get update
+    apt-get upgrade
+    apt-get remove -y oracle*
+    apt-get install -y openjdk-8-jre
+    if [ "`java -version 2>&1 | grep version | grep 1.8.0_65 | wc -l`" -eq "0" ]; then
+        echo -e "\e[92mJava update seems good.\e[39m"
+    else
+        echo -e "\e[91mJava is still 1.8.0_65.\e[39m"
+        echo -e "\e[96mDo you have an active Internet connection?\e[39m"
+        exit 3
+    fi
+else
+    echo -e "\e[92mJava is not the broken version 1.8.0.65.\e[39m";
+fi
+
+if [ "`cat /etc/java-8-openjdk/accessibility.properties | grep ^assistive_technologies | wc -l`" -eq "1" ]; then
+    # Back up config file
+    cp /etc/java-8-openjdk/accessibility.properties /etc/java-8-openjdk/accessibility.properties$(date +%Y%m%d)
+    sed -i -e "s/^assistive_technologies/#assistive_technologies/g" /etc/java-8-openjdk/accessibility.properties
+fi
+
 # Download
 wget https://download.jetbrains.com/python/pycharm-community-2019.1.1.tar.gz ~/
 
@@ -14,7 +40,7 @@ rm ~/pycharm-community-2019.1.1.tar.gz
 if [ -e ~/.local/share/applications/pycharm.desktop ]; then
     echo "Pycharm menu entry already exists"
 else
-    echo <<-EOF > ~/.local/share/applications/pycharm.desktop
+    cat <<-EOF > ~/.local/share/applications/pycharm.desktop
 [Desktop Entry]
 Comment=PyCharm
 Terminal=false
